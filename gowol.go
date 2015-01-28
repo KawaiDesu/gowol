eec58f277642a02402c5c7006e44e8bdbc7f950d
Zmey! <KawaiDesu@users.noreply.github.com>
2015-01-28 08:11:12 +0300

    Add comments and little fixies

--- gowol.go
+++ gowol.go
@@ -13,23 +13,37 @@ var (
 	port  string
 	dhost string
 	mac   string
-	debug bool
+	//debug bool
+)
+
+const (
+	// This should not appear at all while program compiles correctly.
+	ERR_INTERNAL := -1
+	// Something wrong in flags
+	ERR_PARAM    := 1
+	// Network connection problem
+	ERR_NETWORK  := 2
+	
 )
 
 func GenMagickPacket(mac string) []byte {
 	sync_chain, err := hex.DecodeString("FFFFFFFFFFFF")
 	if err != nil {
 		println(err.Error())
+		os.Exit(ERR_INTERNAL)
 	}
+	
 	mac16, err := hex.DecodeString(strings.Repeat(mac, 16))
 	if err != nil {
 		println(err.Error())
+		os.Exit(ERR_INTERNAL)
 	}
+	
+	// Im so noob that i dont know any over fancy way to concat two []byte
 	buf := bytes.NewBuffer(sync_chain)
-	_, err = buf.Write(mac16)
-	if err != nil {
-		println(err.Error())
-	}
+	// First returned value is the length of mac16 (written bytes) - i don't need it; err is always nil.
+	_, _ = buf.Write(mac16)
+
 	return buf.Bytes()
 }
 
@@ -37,26 +51,28 @@ func flags() {
 	flag.StringVar(&port, "port", "9", "set custom destination port")
 	flag.StringVar(&dhost, "host", "", "set destination host")
 	flag.StringVar(&mac, "mac", "", "set MAC address")
+	/* Not implemented yet
 	flag.StringVar(&laddr, "bind", "", "bind to specific network interface")
 	flag.BoolVar(&debug, "debug", false, "turn on debug output")
+	*/
 	flag.Parse()
 	if len(dhost) == 0 || len(mac) == 0 {
 		flag.PrintDefaults()
-		os.Exit(1)
+		os.Exit(ERR_PARAM)
 	}
-}
-
-func atstart() {
-	flags()
-
 }
 
 func main() {
-	atstart()
+	flags()
+	
 	target, err := net.Dial("udp", net.JoinHostPort(dhost, port))
 	if err != nil {
 		println(err.Error())
+		os.Exit(ERR_NETWORK)
 	}
-	target.Write(GenMagickPacket(mac))
+	
+	// IRC-guys said that packet will be sended every Write() call.
+	// So while data < MTU (as our magick packet) it [data] will be sended in single packet (as we need for WOL).
+	target.Write(GenMagickPacket(mac)) 
 	target.Close()
 }
