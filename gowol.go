4b7cacd8eef4ee110226d53ced6cff303f2cad91
KawaiDesu <zmey1992@yandex.com>
2016-03-15 18:53:45 +0300

    total refactoring

--- gowol.go
+++ gowol.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
 	"flag"
 	"net"
-	"strings"
 	"os"
 )
 
-var (
-	port  string
-	dhost string
-	mac   string
-	//debug bool
-)
-
-const (
-	// This should not appear at all while program compiles correctly.
-	ERR_INTERNAL = -1
+const ( 
+	// This should never appear
+	ERR_INTERNAL = 255
 	// Something wrong in flags
 	ERR_PARAM    = 1
 	// Network connection problem
@@ -26,40 +17,48 @@ const (
 	
 )
 
-func GenMagickPacket(mac string) []byte {
-	sync_chain, err := hex.DecodeString("FFFFFFFFFFFF")
-	if err != nil {
-		println(err.Error())
-		os.Exit(ERR_INTERNAL)
-	}
-	
-	mac16, err := hex.DecodeString(strings.Repeat(mac, 16))
-	if err != nil {
-		println(err.Error())
-		os.Exit(ERR_INTERNAL)
-	}
-	
-	// Im so noob that i dont know any over fancy way to concat two []byte
-	buf := bytes.NewBuffer(sync_chain)
-	// First returned value is the length of mac16 (written bytes) - i don't need it; err is always nil.
-	_, _ = buf.Write(mac16)
+var (
+	port  string
+	dhost string
+	mac   net.HardwareAddr
+	//debug bool
+)
 
-	return buf.Bytes()
+func GenMagickPacket([]byte) []byte {
+	sync_chain := []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+// TODO: convert typcal mac to hex-alike format	
+/*
+	mac16, err := hex.DecodeString(strings.Repeat(mac1, 16))
+	if err != nil {
+		println(err.Error())
+		os.Exit(ERR_INTERNAL)
+	}
+*/	
+	return append(sync_chain, bytes.Repeat(mac, 16)...)
 }
 
 func flags() {
 	flag.StringVar(&port, "port", "9", "set custom destination port")
 	flag.StringVar(&dhost, "host", "", "set destination host")
-	flag.StringVar(&mac, "mac", "", "set MAC address")
+	macstring := flag.String("mac", "", "set MAC address")
 	/* Not implemented yet
 	flag.StringVar(&laddr, "bind", "", "bind to specific network interface")
 	flag.BoolVar(&debug, "debug", false, "turn on debug output")
 	*/
 	flag.Parse()
-	if len(dhost) == 0 || len(mac) == 0 {
+
+	if len(dhost) == 0 || len(*macstring) == 0 {
 		flag.PrintDefaults()
 		os.Exit(ERR_PARAM)
 	}
+
+	parsedmac, err := net.ParseMAC(*macstring)
+        if err != nil {
+                println(err.Error())
+                os.Exit(ERR_PARAM)
+        }
+	mac = parsedmac
+
 }
 
 func main() {
@@ -72,7 +71,7 @@ func main() {
 	}
 	
 	// IRC-guys said that packet will be sended every Write() call.
-	// So while data < MTU (as our magick packet) it [data] will be sended in single packet (as we need for WOL).
+	// So while data < MTU (as our magick packet) WOL will be sended in single packet as we need.
 	target.Write(GenMagickPacket(mac)) 
 	target.Close()
 }
