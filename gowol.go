ef782f01d6f6f04815c4792cffbe0b6c04d2dbee
KawaiDesu <zmey1992@yandex.com>
2016-04-13 20:17:06 +0300

    cleanup

--- gowol.go
+++ gowol.go
@@ -7,33 +7,22 @@ import (
 	"os"
 )
 
-const ( 
-	// This should never appear
-	ERR_INTERNAL = 255
+const (
 	// Something wrong in flags
-	ERR_PARAM    = 1
+	ERR_PARAM = 1
 	// Network connection problem
-	ERR_NETWORK  = 2
-	
+	ERR_NETWORK = 2
 )
 
 var (
 	port  string
 	dhost string
 	mac   net.HardwareAddr
-	//debug bool
+	debug bool
 )
 
 func GenMagickPacket([]byte) []byte {
 	sync_chain := []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
-// TODO: convert typcal mac to hex-alike format	
-/*
-	mac16, err := hex.DecodeString(strings.Repeat(mac1, 16))
-	if err != nil {
-		println(err.Error())
-		os.Exit(ERR_INTERNAL)
-	}
-*/	
 	return append(sync_chain, bytes.Repeat(mac, 16)...)
 }
 
@@ -41,10 +30,11 @@ func flags() {
 	flag.StringVar(&port, "port", "9", "set custom destination port")
 	flag.StringVar(&dhost, "host", "", "set destination host")
 	macstring := flag.String("mac", "", "set MAC address")
+
 	/* Not implemented yet
 	flag.StringVar(&laddr, "bind", "", "bind to specific network interface")
-	flag.BoolVar(&debug, "debug", false, "turn on debug output")
 	*/
+
 	flag.Parse()
 
 	if len(dhost) == 0 || len(*macstring) == 0 {
@@ -53,25 +43,24 @@ func flags() {
 	}
 
 	parsedmac, err := net.ParseMAC(*macstring)
-        if err != nil {
-                println(err.Error())
-                os.Exit(ERR_PARAM)
-        }
+	if err != nil {
+		println(err.Error())
+		os.Exit(ERR_PARAM)
+	}
 	mac = parsedmac
-
 }
 
 func main() {
 	flags()
-	
+
 	target, err := net.Dial("udp", net.JoinHostPort(dhost, port))
 	if err != nil {
 		println(err.Error())
 		os.Exit(ERR_NETWORK)
 	}
-	
+
 	// IRC-guys said that packet will be sended every Write() call.
 	// So while data < MTU (as our magick packet) WOL will be sended in single packet as we need.
-	target.Write(GenMagickPacket(mac)) 
+	target.Write(GenMagickPacket(mac))
 	target.Close()
 }
